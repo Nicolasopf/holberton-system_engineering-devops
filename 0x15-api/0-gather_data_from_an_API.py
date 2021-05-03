581c7c82317259d065cf2a8dbccf170c0ceb1bf5
Nicolas <n.ico15935746@gmail.com>
2021-05-03 18:11:02 -0500

    [Added] Using restful api, prints num of tasks completed by an user passed as arg

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Using a rest api return information about the id passed as argument """
+import requests
+from sys import argv
+
+html = requests.get(
+    "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+employee_name = html['name']
+html = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+
+tasks_no_completed = 0
+title_of_tasks = []
+
+for dic in html:
+    if dic['userId'] == int(argv[1]):
+        if dic['completed']:
+            title_of_tasks.append(dic['title'])
+        else:
+            tasks_no_completed += 1
+
+print("Employee {} is done with tasks({}/{}):\n\t {}".format(
+    employee_name, len(title_of_tasks),
+    tasks_no_completed + len(title_of_tasks), "\n\t ".join(title_of_tasks)))
