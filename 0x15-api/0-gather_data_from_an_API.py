8f69905513c39b59c0a6cc2c969afa2bc322d250
Nicolas <n.ico15935746@gmail.com>
2021-05-03 19:05:09 -0500

    [Added] All code entered to name== main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,23 @@
 from sys import argv
 import requests
 
-html = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
-employee_name = html['name']
-html = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+if __name__ == "__main__":
+    html = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])).json()
+    employee_name = html['name']
+    html = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
 
-tasks_no_completed = 0
-title_of_tasks = []
+    tasks_no_completed = 0
+    title_of_tasks = []
 
-for dic in html:
-    if dic['userId'] == int(argv[1]):
-        if dic['completed']:
-            title_of_tasks.append(dic['title'])
-        else:
-            tasks_no_completed += 1
+    for dic in html:
+        if dic['userId'] == int(argv[1]):
+            if dic['completed']:
+                title_of_tasks.append(dic['title'])
+            else:
+                tasks_no_completed += 1
 
-print("Employee {} is done with tasks({}/{}):\n\t {}".format(
-    employee_name, len(title_of_tasks),
-    tasks_no_completed + len(title_of_tasks), "\n\t ".join(title_of_tasks)))
+    print("Employee {} is done with tasks({}/{}):\n\t {}".format(
+        employee_name, len(title_of_tasks),
+        tasks_no_completed + len(title_of_tasks), "\n\t ".join(
+            title_of_tasks)))
