791064716f669b89ace8a1fbc810a6c37a722d49
Nicolas <n.ico15935746@gmail.com>
2021-05-17 15:36:51 -0500

    [Updated] Header user agent in a variable

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ from requests import get
 def number_of_subscribers(subreddit):
     ''' Return the number of suscribers of a subreddit '''
     header = {'User-Agent': "Custom_user"}
-    html = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-               headers=header, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    html = get(url, headers=header, allow_redirects=False)
     if html.status_code == 200:
         return html.json()['data']['suscribers']
     return 0
