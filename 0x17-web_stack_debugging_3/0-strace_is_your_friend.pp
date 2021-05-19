9abdfe85cbc1846fe8bf9ff72e043a6a8a96adc3
Nicolas <n.ico15935746@gmail.com>
2021-05-19 17:16:35 -0500

    [Updated] Simple quotes to double

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Execute command to fix typo in wp-settings
 exec { 'sed':
-  command => 'sed -i 's/phpp/php/' /var/www/html/wp-settings.php',
-  path    => ['/usr/bin', '/bin'],
+  command 	  => 'sed -i "s/phpp/php/" $DIR',
+  path    	  => ['/usr/bin', '/bin'],
 }
