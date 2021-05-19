54fe590a800f230b2b4444b2be9a4c3406081bb6
Nicolas <n.ico15935746@gmail.com>
2021-05-19 17:20:33 -0500

    [Updated] Function name changed and sudo added to command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Execute command to fix typo in wp-settings
-exec { 'sed':
-  command => 'sed -i "s/phpp/php/" $DIR',
+exec { 'fix':
+  command => 'sudo sed -i "s/phpp/php/" $DIR',
   path    => ['/usr/bin', '/bin'],
   returns => [0, 1],
 }
