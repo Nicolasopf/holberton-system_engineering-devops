077732c06404fca2869c3c94366dac7ee7d655d7
Nicolas <n.ico15935746@gmail.com>
2021-06-28 01:29:39 -0500

    [Added] File to install nginx from zero using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Configuring nginx server from zero with puppet
+
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Holberton School'
+}
+->
+file_line { 'Redirection':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;'
+}
+->
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx']
+}
